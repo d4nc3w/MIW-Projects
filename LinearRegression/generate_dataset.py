b4924529b3eaceb12cbc4541289579d612e3f6a4
Piotr <piotr@dancewicz.pl>
2025-04-05 14:49:35 +0200

    Added basic exception for arguments

--- LinearRegression/generate_dataset.py
+++ LinearRegression/generate_dataset.py
@@ -5,9 +5,11 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', '--output', type=str, required=True, help='Output csv file')
     parser.add_argument('-i', '--input', nargs='+', default=[], help='Input csv files')
-    parser.add_argument('-s', '--long_name', nargs=2, default=[])
 
-    return parser.parse_args()
+    args = parser.parse_args()
+    if not args.input:
+        raise ValueError('Cannot start without specified input csv files')
+    return args
 
 def main(args: argparse.Namespace)-> None:
     print(f'Output file: {args.output}')
@@ -22,5 +24,6 @@ def main(args: argparse.Namespace)-> None:
 if __name__ == '__main__':
     main(parse_args())
 
+#Todo: add exceptions
 
-
+# python generate_dataset.py -o dataset.csv -i coin_Monero.csv coin_Dogecoin.csv coin_Bitcoin.csv
